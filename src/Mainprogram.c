eb2f7f63bdabd65fb6dcf5548be23861f47f81e3
Skool17 <semen9422@gmail.com>
2017-06-27 18:36:46 +0700

    Update

--- src/Mainprogram.c
+++ src/Mainprogram.c
@@ -10,14 +10,6 @@ int main()
 	scanf("%f",&a);
 	scanf("%f",&b);
 	scanf("%f",&c);
-	if (a == 0) && (b == 0) {
-		printf("Net kornei");
-		return 0;
-	}
-	if (a == 0) {
-		x = c * -1 / b;
-		printf("%7.2f",x);
-		return 0;
 	D=b*b-4*a*c;
 	function(a,b,c,D);
 }
